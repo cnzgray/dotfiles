78a55772c4b2d7c43c5ce10df358bcb9be28cfee
cnzgray <cnzgray@qq.com>
2019-08-22 09:23:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,6 @@ echo " ********************************************************"
 
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# install zsh plugins
-ZSH_CUSTOM="$HOME/.dotfiles/ohmyzsh/custom"
-mkdir -p $ZSH_CUSTOM
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
 
 echo " ********************************************************"
 echo " ************ INFO: install dotfiles  *******************"
@@ -21,6 +15,13 @@ echo " ********************************************************"
 dir="$HOME/.dotfiles"
 mkdir -p $dir
 git clone --recursive https://github.com/cnzgray/dotfiles.git $dir
-cd $dir
 
+# install zsh plugins
+ZSH_CUSTOM="$HOME/.dotfiles/ohmyzsh/custom"
+mkdir -p $ZSH_CUSTOM
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
+
+cd $dir
 sh etc/symlink-dotfiles.sh
