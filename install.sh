7e44b849959670c8dd3eea5c53d55ce3f055bc7f
cnzgray <cnzgray@qq.com>
2019-08-22 09:32:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,14 +14,28 @@ echo " ************ INFO: install dotfiles  *******************"
 echo " ********************************************************"
 dir="$HOME/.dotfiles"
 mkdir -p $dir
-git clone --recursive https://github.com/cnzgray/dotfiles.git $dir
+if [ ! -d "$dir" ]; then
+    git clone --recursive https://github.com/cnzgray/dotfiles.git $dir
+fi
 
 # install zsh plugins
 ZSH_CUSTOM="$HOME/.dotfiles/ohmyzsh/custom"
 mkdir -p $ZSH_CUSTOM
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+fi
+
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting
+fi
+
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-completions" ]; then
+  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM}/plugins/zsh-completions
+fi
+
+if [ ! -d "${ZSH_CUSTOM}/plugins/zsh-nvm" ]; then
+  git clone https://github.com/lukechilds/zsh-nvm ${ZSH_CUSTOM}/plugins/zsh-nvm
+fi
 
 cd $dir
 sh etc/symlink-dotfiles.sh
